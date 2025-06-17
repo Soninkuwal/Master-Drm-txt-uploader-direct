3e8671a4b9c19de4e60a5b3c71f48bcb8cce4eac
sonic kuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-06-17 15:09:56 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,9 +3,10 @@
 import os
 from os import environ
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = os.environ.get("API_ID", "24473318")
-    API_HASH = os.environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5")
+    
+API_ID = os.environ.get("API_ID", "24473318")
+API_HASH = os.environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") #Replace with your bot token
 OWNER = int(environ.get("OWNER", "7841292070"))
 CREDIT = "𝄟⃝‌🐬🇳‌ɪᴋʜɪʟ𝄟⃝🐬"
 AUTH_USER = os.environ.get('AUTH_USERS', '7841292070,-1002624447857,-1002260543763,-1002501547365,-1002578436500,-1002780494901,-1002487629618').split(',')
